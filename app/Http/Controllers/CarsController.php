4ea83dd50a321feb6550b8d4d41c49b9e4bb4462
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-04-06 16:03:39 +0300

    create controller

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cars;
+use Illuminate\Http\Request;
+
+class CarsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Cars::paginate();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        var_dump(1);
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cars  $cars
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cars $cars)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cars  $cars
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cars $cars)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cars  $cars
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cars $cars)
+    {
+        //
+    }
+}
